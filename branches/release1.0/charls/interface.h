cc2a6e8638ce06ef6eeb14c0fae3e5b8e1e5e855
jdv_cp <SND\jdv_cp>
2011-01-11 14:08:41 -0800

    simplify static linking on windows

--- branches/release1.0/charls/interface.h
+++ branches/release1.0/charls/interface.h
@@ -8,15 +8,24 @@
 
 #include "publictypes.h"
 
-#if defined(_WIN32)
-#ifndef CHARLS_IMEXPORT
-#define CHARLS_IMEXPORT(returntype) __declspec(dllimport) returntype __stdcall
+// non-windows (static linking)
+#if !defined(CHARLS_IMEXPORT) && !defined(_WIN32)
+#  define CHARLS_IMEXPORT(returntype) returntype
 #endif
-#else
-#ifndef CHARLS_IMEXPORT 
-#define CHARLS_IMEXPORT(returntype) returntype
+
+// windows static linking
+#if !defined(CHARLS_IMEXPORT) && defined(CHARLS_STATIC)
+#  define CHARLS_IMEXPORT(returntype) returntype
 #endif
-#endif /* _WIN32 */
+
+// windows dll
+#if !defined(CHARLS_IMEXPORT) && defined(CHARLS_DLL)
+#  define CHARLS_IMEXPORT(returntype) __declspec(dllimport) returntype __stdcall
+#endif
+
+#if !defined(CHARLS_IMEXPORT)
+#error Please #define CHARLS_STATIC or CHARLS_DLL before including "interface.h" to indicate if CharLS is built as a static library or as a dll. 
+#endif
 
 
 #ifdef __cplusplus
