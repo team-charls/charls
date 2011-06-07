75d85a8775daaf98925b5263a907420f4fdf8388
Victor Derks <SND\vbaderks_cp>
2011-06-07 13:35:41 -0700

    Added unittest to increased logical test coverage. Still need to figure out why decompress test fails.

--- trunk/charls/nettest/JpegLSCodecTest.cs
+++ trunk/charls/nettest/JpegLSCodecTest.cs
@@ -2,38 +2,60 @@
 // (C) Jan de Vaan 2007-2011, all rights reserved. See the accompanying "License.txt" for licensed use.
 //
 
-using System;
+using System.IO;
 using NUnit.Framework;
 
 namespace CharLS.Test
 {
-    using System.IO;
-
     [TestFixture]
     public class JpegLSCodecTest
     {
         [Test]
-        public void GetMetadataInfo()
+        public void GetMetadataInfoFromLosslessEncodedColorImage()
         {
             var source = ReadAllBytes("T8C0E0.JLS");
             var info = JpegLSCodec.GetMetadataInfo(source);
+            var expected = new JpegLSMetadataInfo { Height = 256, Width = 256, BitsPerSample = 8, ComponentCount = 3 };
 
-            Assert.AreEqual(256, info.Width);
-            Assert.AreEqual(256, info.Height);
-            Assert.AreEqual(3, info.ComponentCount);
+            Assert.AreEqual(expected, info);
+        }
+
+        [Test]
+        public void GetMetadataInfoFromNearLosslessEncodedColorImage()
+        {
+            var source = ReadAllBytes("T8C0E3.JLS");
+            var info = JpegLSCodec.GetMetadataInfo(source);
+            var expected = new JpegLSMetadataInfo { Height = 256, Width = 256, BitsPerSample = 8, ComponentCount = 3, AllowedLossyError = 3 };
+
+            Assert.AreEqual(expected, info);
         }
 
         [Test]
         public void Decompress()
         {
             var source = ReadAllBytes("T8C0E0.JLS");
+            var expected = ReadAllBytes("TEST8.PPM", 15);
             var uncompressed = JpegLSCodec.Decompress(source);
+
+            //// TODO: resolve issue why decompressed data doesn't match expected.
+            ////Assert.AreEqual(expected, uncompressed);
         }
 
-        private static byte[] ReadAllBytes(string path)
+        private static byte[] ReadAllBytes(string path, int bytesToSkip = 0)
         {
-            return File.ReadAllBytes("DataFiles\\" + path);
+            var fullPath = "DataFiles\\" + path;
+
+            if (bytesToSkip == 0)
+                return File.ReadAllBytes(fullPath);
+
+            using (var stream = File.OpenRead(fullPath))
+            {
+                var result = new byte[new FileInfo(fullPath).Length - bytesToSkip];
+
+                stream.Seek(bytesToSkip, SeekOrigin.Begin);
+                stream.Read(result, 0, result.Length);
+                return result;
+            }
         }
     }
-
 }
---
+++ trunk/charls/nettest/JpegLSMetadataInfoTest.cs
@@ -0,0 +1,56 @@
+﻿//
+// (C) Jan de Vaan 2007-2011, all rights reserved. See the accompanying "License.txt" for licensed use.
+//
+
+using NUnit.Framework;
+
+namespace CharLS.Test
+{
+    [TestFixture]
+    public class JpegLSMetadataInfoTest
+    {
+        [Test]
+        public void ConstructDefault()
+        {
+            var info = new JpegLSMetadataInfo();
+
+            Assert.AreEqual(0, info.Width);
+            Assert.AreEqual(0, info.Height);
+            Assert.AreEqual(0, info.ComponentCount);
+            Assert.AreEqual(0, info.BitsPerSample);
+        }
+
+        [Test]
+        public void EquatableSameObjects()
+        {
+            var a = new JpegLSMetadataInfo();
+            var b = new JpegLSMetadataInfo();
+
+            Assert.IsTrue(a.Equals(b));
+            Assert.IsTrue(a.Equals((object)b));
+            Assert.AreEqual(a, b);
+            Assert.AreEqual(b, a);
+            Assert.AreEqual(a.GetHashCode(), b.GetHashCode());
+        }
+
+        [Test]
+        public void EquatableDifferentObjects()
+        {
+            var a = new JpegLSMetadataInfo();
+            var b = new JpegLSMetadataInfo { Height = 1 };
+
+            Assert.IsFalse(a.Equals(b));
+            Assert.IsFalse(a.Equals((object)b));
+        }
+
+        [Test]
+        public void EquatableWithNull()
+        {
+            var a = new JpegLSMetadataInfo();
+            JpegLSMetadataInfo b = null;
+
+            Assert.IsFalse(a.Equals(b));
+            Assert.IsFalse(a.Equals((object)null));
+        }
+    }
+}
