8fe9871bccc57e20e85669a371df0be7a778b5dc
Victor Derks <SND\vbaderks_cp>
2011-06-07 13:34:38 -0700

    Added support for IEquatable<JpegLSMetadataInfo.

--- trunk/charls/net/JpegLSMetadataInfo.cs
+++ trunk/charls/net/JpegLSMetadataInfo.cs
@@ -2,12 +2,14 @@
 // (C) Jan de Vaan 2007-2011, all rights reserved. See the accompanying "License.txt" for licensed use.
 //
 
+using System;
+
 namespace CharLS
 {
     /// <summary>
     /// Contains meta information about a compressed JPEG-LS stream or info how to compress.
     /// </summary>
-    public class JpegLSMetadataInfo
+    public class JpegLSMetadataInfo : IEquatable<JpegLSMetadataInfo>
     {
         /// <summary>
         /// Initializes a new instance of the <see cref="JpegLSMetadataInfo"/> class.
@@ -16,11 +18,13 @@ namespace CharLS
         {
         }
 
-        internal JpegLSMetadataInfo(ref JlsParameters jlsParameters)
+        internal JpegLSMetadataInfo(ref JlsParameters parameters)
         {
-            Width = jlsParameters.Width;
-            Height = jlsParameters.Height;
-            ComponentCount = jlsParameters.Components;
+            Width = parameters.Width;
+            Height = parameters.Height;
+            ComponentCount = parameters.Components;
+            BitsPerSample = parameters.BitsPerSample;
+            AllowedLossyError = parameters.AllowedLossyError;
         }
 
         /// <summary>
@@ -68,6 +72,43 @@ namespace CharLS
             get
             {
                 return Width * Height * ComponentCount * ((BitsPerSample + 7) / 8);
+            }
+        }
+
+        public override string ToString()
+        {
+            return string.Format("Width = {0}, Height = {1}, BitsPerSample = {2}, ComponentCount = {3}, AllowedLossyError = {4}",
+                Width, Height, BitsPerSample, ComponentCount, AllowedLossyError);
+        }
+
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as JpegLSMetadataInfo);
+        }
+
+        public bool Equals(JpegLSMetadataInfo other)
+        {
+            if (other == null)
+                return false;
+
+            return Width == other.Width &&
+                   Height == other.Height &&
+                   ComponentCount == other.ComponentCount &&
+                   BitsPerSample == other.BitsPerSample &&
+                   AllowedLossyError == other.AllowedLossyError;
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                int result = this.Width;
+                result = (result * 397) ^ this.Height;
+                result = (result * 397) ^ this.BitsPerSample;
+                result = (result * 397) ^ this.BytesPerLine;
+                result = (result * 397) ^ this.ComponentCount;
+                result = (result * 397) ^ this.AllowedLossyError;
+                return result;
             }
         }
     }
