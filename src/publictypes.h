2f95a7513635c1d896fbc08ba4edfe1b8c8d4fb7
Victor Derks <vbaderks@gmail.com>
2015-04-11 20:54:29 +0200

    Added documentation to the InterleaveMode enum options.

--- src/publictypes.h
+++ src/publictypes.h
@@ -28,10 +28,24 @@ enum JLS_ERROR
 
 namespace charls
 {
+    /// <summary>
+    /// Defines the interleave mode for multi-component (color) pixel data.
+    /// </summary>
     enum class InterleaveMode
     {
+        /// <summary>
+        /// The data is encoded and stored as component for component: RRRGGGBBB.
+        /// </summary>
         None   = 0,
+
+        /// <summary>
+        /// The interleave mode is by line. A full line of each component is encoded before moving to the next line.
+        /// </summary>
         Line   = 1,
+
+        /// <summary>
+        /// The data is encoded and stored by sample. For color images this is the format like RGBRGBRGB.
+        /// </summary>
         Sample = 2
     };
 }
