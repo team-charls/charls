1de019ee8d898db8be269ac39107b63ff20d0304
Victor Derks <vbaderks@gmail.com>
2015-04-19 11:10:36 +0200

    Corrected typo in comment statement

--- src/jpegmarkercode.h
+++ src/jpegmarkercode.h
@@ -30,11 +30,11 @@ enum class JpegMarkerCode : uint8_t
     StartOfFrameProgressiveArithemtic = 0xCA,   // SOF_10: Marks the start of a (progressive, arithmetic) encoded frame.
     StartOfFrameLosslessArithemtic = 0xCB,      // SOF_11: Marks the start of a (lossless, arithmetic) encoded frame.
 
-    StartOfFrameJpegLS = 0xF7,             // SOF_55: Marks the start of a (JPEG-LS) encoded frame.
-    JpegLSExtendedParameters = 0xF8,       // LSE:    JPEG-LS extended parameters.
+    StartOfFrameJpegLS = 0xF7,                  // SOF_55: Marks the start of a (JPEG-LS) encoded frame.
+    JpegLSExtendedParameters = 0xF8,            // LSE:    JPEG-LS extended parameters.
 
-    ApplicationData0 = 0xE0, // APP0: Application data 0: used for JFIF header.
-    ApplicationData7 = 0xE7, // APP8: Application data 7: colorspace.
-    ApplicationData8 = 0xE8, // APP8: Application data 8: colorXForm.
-    Comment = 0xFE           // COM:  Comment block.
+    ApplicationData0 = 0xE0,                    // APP0: Application data 0: used for JFIF header.
+    ApplicationData7 = 0xE7,                    // APP7: Application data 7: colorspace.
+    ApplicationData8 = 0xE8,                    // APP8: Application data 8: colorXForm.
+    Comment = 0xFE                              // COM:  Comment block.
 };
