5304ea10103b61aed75df50ae62df9d5f61f08e7
Victor Derks <vbaderks@gmail.com>
2015-04-26 23:24:22 +0200

    Added API documentation to the struct JlsParameters.

--- src/publictypes.h
+++ src/publictypes.h
@@ -135,14 +135,55 @@ struct JfifParameters
 
 struct JlsParameters
 {
+    /// <summary>
+    /// Width of the image in pixels.
+    /// </summary>
     int width;
+
+    /// <summary>
+    /// Height of the image in pixels.
+    /// </summary>
     int height;
+
+    /// <summary>
+    /// The number of valid bits per sample to encode.
+    /// Valid range 2 - 16. When greater than 8, pixels are assumed to stored as two bytes per sampe, otherwise one byte per sample is assumed.
+    /// </summary>
     int bitspersample;
+
+    /// <summary>
+    /// The stride is the number of bytes from one row of pixels in memory to the next row of pixels in memory.
+    /// Stride is sometimes called pitch. If padding bytes are present, the stride is wider than the width of the image.
+    /// </summary>
     int bytesperline;	/* for [source (at encoding)][decoded (at decoding)] pixel image in user buffer */
+
+    /// <summary>
+    /// The number of components.
+    /// Typical 1 for monochrome images and 3 for color images or 4 if alpha channel is present.
+    /// </summary>
     int components;
+
+    /// <summary>
+    /// Defines the allowed lossy error. Value 0 defines lossless.
+    /// </summary>
     int allowedlossyerror;
+
+    /// <summary>
+    /// Determines the order of the color components in the compressed stream.
+    /// </summary>
     CharlsInterleaveModeType ilv;
+
+    /// <summary>
+    /// Color transformation used in the compressed stream. The color transformations are all lossless and 
+    /// are an HP proprietary extension of the standard. Do not use the color transformations unless 
+    /// you know the decoder is capable of decoding it. Color transform typically improve compression ratios only 
+    /// for sythetic images (non - photorealistic computer generated images).
+    /// </summary>
     CharlsColorTransformationType colorTransform;
+
+    /// <summary>
+    /// If set to true RGB images will be decoded to BGR. BGR is the standard ordering in MS Windows bitmaps.
+    /// </summary>
     char outputBgr;
     struct JlsCustomParameters custom;
     struct JfifParameters jfif;
