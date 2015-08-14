125b2ce0ff22daceab1d2b83796790e5123d54b8
vbaderks <vbaderks@gmail.com>
2015-08-14 22:04:03 +0200

    Included class JpegStreamReaderTest to unit test project.

--- unittest/jpegstreamreadertest.cpp
+++ unittest/jpegstreamreadertest.cpp
@@ -10,27 +10,6 @@
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
-// Verify that a function raises an exception:
-template<typename _EXPECTEDEXCEPTION, typename _FUNCTOR> static _EXPECTEDEXCEPTION Assert_ExpectException(_FUNCTOR functor, const wchar_t* message = NULL, const __LineInfo* pLineInfo = NULL)
-{
-    try
-    {
-        functor();
-    }
-    catch (_EXPECTEDEXCEPTION const &error)
-    {
-        return error;
-    }
-    catch (...)
-    {
-        Assert::Internal_SetExpectedExceptionMessage(reinterpret_cast<const unsigned short *>(message));
-        throw;
-    }
-
-    Assert::Fail(message, pLineInfo);
-    return _EXPECTEDEXCEPTION();
-}
-
 namespace CharLSUnitTest
 {
     TEST_CLASS(JpegStreamReaderTest)
