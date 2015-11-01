2386b45861561eccb9cef26bfe5f2ef6e3164fb1
Victor Derks <vbaderks@gmail.com>
2015-11-01 17:49:42 +0100

    Added unit test AppendToBitStreamFFPattern. It triggers the bit stream bug (assert goes off).

--- unittest/encoderstrategytest.cpp
+++ unittest/encoderstrategytest.cpp
@@ -1,4 +1,4 @@
-//
+﻿//
 // (C) CharLS Team 2014, all rights reserved. See the accompanying "License.txt" for licensed use. 
 //
 
@@ -13,22 +13,27 @@ using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 class EncoderStrategyTester : EncoderStrategy
 {
 public:
-    EncoderStrategyTester(const JlsParameters& info) : EncoderStrategy(info)
+    explicit EncoderStrategyTester(const JlsParameters& info) : EncoderStrategy(info)
     {
     }
 
-    virtual void SetPresets(const JlsCustomParameters&)
+    virtual void SetPresets(const JlsCustomParameters&) override
     {
     }
 
-    virtual size_t EncodeScan(std::unique_ptr<ProcessLine>, ByteStreamInfo&, void*)
+    virtual size_t EncodeScan(std::unique_ptr<ProcessLine>, ByteStreamInfo&, void*) override
     {
         return 0;
     }
 
-    virtual ProcessLine* CreateProcess(ByteStreamInfo)
+    virtual ProcessLine* CreateProcess(ByteStreamInfo) override
     {
         return nullptr;
+    }
+
+    void InitTest(ByteStreamInfo& info)
+    {
+        Init(info);
     }
 
     void AppendToBitStreamTest(int32_t value, int32_t length)
@@ -48,14 +53,52 @@ namespace CharLSUnitTest
     TEST_CLASS(EncoderStrategyTest)
     {
     public:
-        TEST_METHOD(AppendToBitStream)
+        TEST_METHOD(AppendToBitStreamZeroLength)
         {
             JlsParameters info;
 
-            EncoderStrategyTester es(info);
+            EncoderStrategyTester strategy(info);
 
-            es.AppendToBitStreamTest(0, 0);
-            es.FlushTest();
+            uint8_t data[1024];
+
+            ByteStreamInfo stream;
+            stream.rawStream = nullptr;
+            stream.rawData = data;
+            stream.count = sizeof(data);
+            strategy.InitTest(stream);
+
+            strategy.AppendToBitStreamTest(0, 0);
+            strategy.FlushTest();
+        }
+
+        TEST_METHOD(AppendToBitStreamFFPattern)
+        {
+            // Failing unit test. It exposes the reported bug that AppendToBitStream has a flaw for a certain bit pattern.
+            return;
+
+            JlsParameters info;
+
+            EncoderStrategyTester strategy(info);
+
+            uint8_t data[1024];
+
+            ByteStreamInfo stream;
+            stream.rawStream = nullptr;
+            stream.rawData = data;
+            stream.count = sizeof(data);
+            strategy.InitTest(stream);
+
+            // We want _isFFWritten == true.
+            strategy.AppendToBitStreamTest(0, 24);
+            strategy.AppendToBitStreamTest(0xff, 8);
+
+            // We need the buffer filled with set bits.
+            strategy.AppendToBitStreamTest(0xffff, 16);
+            strategy.AppendToBitStreamTest(0xffff, 16);
+
+            strategy.AppendToBitStreamTest(0, 31);
+
+            //strategy.FlushTest();
         }
     };
 }
