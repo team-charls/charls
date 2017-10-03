bd8cb5c43ad12c41ea9a49cbbe36fb99c02097f2
Victor Derks <vbaderks@gmail.com>
2017-10-03 12:59:02 +0200

    Fix issue #35 for the 1.x branch

--- src/jpegls.cpp
+++ src/jpegls.cpp
@@ -1,6 +1,6 @@
-// 
-// (C) Jan de Vaan 2007-2010, all rights reserved. See the accompanying "License.txt" for licensed use. 
-// 
+//
+// (C) Jan de Vaan 2007-2010, all rights reserved. See the accompanying "License.txt" for licensed use.
+//
 
 #include "config.h"
 #include "util.h"
@@ -12,9 +12,9 @@
 #include <iostream>
 
 
-// As defined in the JPEG-LS standard 
+// As defined in the JPEG-LS standard
 
-// used to determine how large runs should be encoded at a time. 
+// used to determine how large runs should be encoded at a time.
 const int J[32]         = {0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 9, 10, 11, 12, 13, 14, 15};
 
 
@@ -34,7 +34,7 @@ signed char QuantizeGratientOrg(const JlsCustomParameters& preset, LONG NEAR, LO
     if (Di < preset.T1)   return   1;
     if (Di < preset.T2)   return   2;
     if (Di < preset.T3)   return   3;
-    
+
     return  4;
 }
 
@@ -46,7 +46,7 @@ std::vector<signed char> CreateQLutLossless(LONG cbit)
     LONG range = preset.MAXVAL + 1;
 
     std::vector<signed char> lut(range * 2);
-    
+
     for (LONG diff = -range; diff < range; diff++)
     {
         lut[range + diff] = QuantizeGratientOrg(preset, 0,diff);
@@ -59,13 +59,13 @@ std::vector<signed char> CreateQLutLossless(LONG cbit)
 
 
 // Lookup table: decode symbols that are smaller or equal to 8 bit (16 tables for each value of k)
-CTable decodingTables[16] = { InitTable(0), InitTable(1), InitTable(2), InitTable(3), 
-                             InitTable(4), InitTable(5), InitTable(6), InitTable(7), 
-                             InitTable(8), InitTable(9), InitTable(10), InitTable(11), 
+CTable decodingTables[16] = { InitTable(0), InitTable(1), InitTable(2), InitTable(3),
+                             InitTable(4), InitTable(5), InitTable(6), InitTable(7),
+                             InitTable(8), InitTable(9), InitTable(10), InitTable(11),
                              InitTable(12), InitTable(13), InitTable(14),InitTable(15) };
 
 
-// Lookup tables: sample differences to bin indexes. 
+// Lookup tables: sample differences to bin indexes.
 std::vector<signed char> rgquant8Ll = CreateQLutLossless(8);
 std::vector<signed char> rgquant10Ll = CreateQLutLossless(10);
 std::vector<signed char> rgquant12Ll = CreateQLutLossless(12);
@@ -78,10 +78,20 @@ std::auto_ptr<STRATEGY> JlsCodecFactory<STRATEGY>::GetCodec(const JlsParameters&
     STRATEGY* pstrategy;
     if (presets.RESET != 0 && presets.RESET != BASIC_RESET)
     {
-        DefaultTraitsT<BYTE,BYTE> traits((1 << info.bitspersample) - 1, info.allowedlossyerror); 
-        traits.MAXVAL = presets.MAXVAL;
-        traits.RESET = presets.RESET;
-        pstrategy = new JlsCodec<DefaultTraitsT<BYTE, BYTE>, STRATEGY>(traits, info); 
+        if (info.bitspersample <= 8)
+        {
+            DefaultTraitsT<BYTE, BYTE> traits((1 << info.bitspersample) - 1, info.allowedlossyerror);
+            traits.MAXVAL = presets.MAXVAL;
+            traits.RESET = presets.RESET;
+            pstrategy = new JlsCodec<DefaultTraitsT<BYTE, BYTE>, STRATEGY>(traits, info);
+        }
+        else
+        {
+            DefaultTraitsT<USHORT, USHORT> traits((1 << info.bitspersample) - 1, info.allowedlossyerror);
+            traits.MAXVAL = presets.MAXVAL;
+            traits.RESET = presets.RESET;
+            pstrategy = new JlsCodec<DefaultTraitsT<USHORT, USHORT>, STRATEGY>(traits, info);
+        }
     }
     else
     {
@@ -105,7 +115,7 @@ STRATEGY* CreateCodec(const TRAITS& t, const STRATEGY*,const JlsParameters& info
 
 template<class STRATEGY>
 STRATEGY* JlsCodecFactory<STRATEGY>::GetCodecImpl(const JlsParameters& info)
-{   
+{
     STRATEGY* s = 0;
 
     if (info.ilv == ILV_SAMPLE && info.components != 3)
@@ -115,7 +125,7 @@ STRATEGY* JlsCodecFactory<STRATEGY>::GetCodecImpl(const JlsParameters& info)
 
     // optimized lossless versions common formats
     if (info.allowedlossyerror == 0)
-    {       
+    {
         if (info.ilv == ILV_SAMPLE)
         {
             if (info.bitspersample == 8)
@@ -125,7 +135,7 @@ STRATEGY* JlsCodecFactory<STRATEGY>::GetCodecImpl(const JlsParameters& info)
         {
             switch (info.bitspersample)
             {
-                case  8: return CreateCodec(LosslessTraitsT<BYTE,    8>(), s, info); 
+                case  8: return CreateCodec(LosslessTraitsT<BYTE,    8>(), s, info);
                 case 12: return CreateCodec(LosslessTraitsT<USHORT, 12>(), s, info);
                 case 16: return CreateCodec(LosslessTraitsT<USHORT, 16>(), s, info);
             }
@@ -140,7 +150,7 @@ STRATEGY* JlsCodecFactory<STRATEGY>::GetCodecImpl(const JlsParameters& info)
     {
         if (info.ilv == ILV_SAMPLE)
             return CreateCodec(DefaultTraitsT<BYTE,Triplet<BYTE> >(maxval, info.allowedlossyerror), s, info);
-        
+
         return CreateCodec(DefaultTraitsT<BYTE, BYTE>((1 << info.bitspersample) - 1, info.allowedlossyerror), s, info);
     }
 
