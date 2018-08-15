e41d82e7397d5019e5b94e29d82dc2333598fd79
Victor Derks <vbaderks@gmail.com>
2018-08-15 21:33:47 +0200

    Disable conformanceMode to compile CppUnitTest.h with MSVC 15.8.0
    CppUnitTest.h cannot be compiled with conformanceMode enabled in MSVC 15.8.0. Known issue, will be fixed in 15.9.0 Preview1

--- unittest/stdafx.h
+++ unittest/stdafx.h
@@ -10,8 +10,10 @@
 #pragma warning(disable : 26439)
 #pragma warning(disable : 26440)
 #pragma warning(disable : 26443)
+#pragma warning(disable : 26455) // Default constructor may not throw. Declare it 'noexcept' (f.6). [Problem in VS 2017 15.8.0]
 #pragma warning(disable : 26461)
 #pragma warning(disable : 26466)
+#pragma warning(disable : 26477) // Use 'nullptr' rather than 0 or NULL (es.47) [Problem in VS 2017 15.8.0]
 #pragma warning(disable : 26495)
 #pragma warning(disable : 26496)
 #include <CppUnitTest.h>
