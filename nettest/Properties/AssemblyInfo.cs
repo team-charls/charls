cb6d12271b003899db6f0b6273ee6ccb2d078d4a
Victor Derks <vbaderks@gmail.com>
2018-11-21 21:50:48 +0100

    Replace the CharLSNetTest.csproj format with the new .NET project format.
    Note: it is not possible to use .NET Core for this unit test project. Only 1 dotnet.exe can be in the path, and while both the x64 and x86 version can be installed, only 1 can be active. This makes it impossible to test both the x64 and the x86 build.
    
    Other changes:
    - Remove JpegLSBitmapDecoder (unsupported by .NET Core)
    - Rename solution name from Win32 to x86 to ensure CI build works.

--- nettest/Properties/AssemblyInfo.cs
+++ nettest/Properties/AssemblyInfo.cs
@@ -1,18 +1,11 @@
-﻿// Copyright (c) Team CharLS. All rights reserved. See the accompanying "LICENSE.md" for licensed use.
+// Copyright (c) Team CharLS. All rights reserved. See the accompanying "LICENSE.md" for licensed use.
 
 using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("CharLSNetTest")]
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("CharLS")]
 [assembly: AssemblyCopyright("Copyright (c) Team CharLS")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
-
-[assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
