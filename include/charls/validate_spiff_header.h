f1d2f691b1e8a52f4acff7f8b2dbe957a8abde6e
Victor Derks <vbaderks@gmail.com>
2022-05-27 16:38:04 +0200

    Add documentation to charls_validate_spiff_header (#190)

--- include/charls/validate_spiff_header.h
+++ include/charls/validate_spiff_header.h
@@ -9,6 +9,17 @@
 extern "C" {
 #endif
 
+/// <summary>
+/// Validates a SPIFF header. It is validated against the ISO/IEC 10918-3, Annex F and if the
+/// values match with the frame info argument.
+/// </summary>
+/// <remarks>
+/// ISO/IEC 10918-3 (SPIFF) and ISO/IEC 14495-1 (JPEG-LS) use different allowed ranges for
+/// bits per sample: (1, 2, 4, 8, 12, 16) vs [2..16]. The JPEG-LS range is used for validation.
+/// </remarks>
+/// <param name="spiff_header">Reference to a charls_spiff_header instance.</param>
+/// <param name="frame_info">Reference to a charls_frame_info instance.</param>
+/// <returns>charls_jpegls_errc::success when the SPIFF header is valid, otherwise charls_jpegls_errc::invalid_spiff_header.</returns>
 CHARLS_CHECK_RETURN CHARLS_API_IMPORT_EXPORT charls_jpegls_errc CHARLS_API_CALLING_CONVENTION charls_validate_spiff_header(
     CHARLS_IN const charls_spiff_header* spiff_header, CHARLS_IN const charls_frame_info* frame_info) CHARLS_NOEXCEPT
     CHARLS_ATTRIBUTE((nonnull));
