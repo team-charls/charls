0d507062cdc30c234acef9194be04e0e7417b710
Victor Derks <vbaderks@gmail.com>
2025-07-29 21:58:51 +0200

    Add explicit inline to constexpr namespace variables (#346)
    The C++17 specs are not very clear, but inline is not implicit for a constexpr namespace variable. Add it explicit to ensure no ODR violations.

--- src/constants.hpp
+++ src/constants.hpp
@@ -9,50 +9,50 @@
 
 namespace charls {
 
-constexpr int32_t default_reset_threshold{64}; // Default RESET value as defined in ISO/IEC 14495-1, table C.2
+inline constexpr int32_t default_reset_threshold{64}; // Default RESET value as defined in ISO/IEC 14495-1, table C.2
 
-constexpr int32_t minimum_component_count{1};
-constexpr int32_t maximum_component_count{255};
-constexpr int32_t maximum_component_count_in_scan{4};
-constexpr int32_t minimum_component_index{};
-constexpr int32_t maximum_component_index{maximum_component_count - 1};
-constexpr int32_t minimum_bits_per_sample{2};
-constexpr int32_t maximum_bits_per_sample{16};
-constexpr int32_t maximum_near_lossless{255};
-constexpr int32_t minimum_application_data_id{};
-constexpr int32_t maximum_application_data_id{15};
+inline constexpr int32_t minimum_component_count{1};
+inline constexpr int32_t maximum_component_count{255};
+inline constexpr int32_t maximum_component_count_in_scan{4};
+inline constexpr int32_t minimum_component_index{};
+inline constexpr int32_t maximum_component_index{maximum_component_count - 1};
+inline constexpr int32_t minimum_bits_per_sample{2};
+inline constexpr int32_t maximum_bits_per_sample{16};
+inline constexpr int32_t maximum_near_lossless{255};
+inline constexpr int32_t minimum_application_data_id{};
+inline constexpr int32_t maximum_application_data_id{15};
 
 // The following limits for mapping tables are defined in ISO/IEC 14495-1, C.2.4.1.2, table C.4.
-constexpr int32_t minimum_mapping_table_id{1};
-constexpr int32_t maximum_mapping_table_id{255};
-constexpr int32_t minimum_mapping_entry_size{1};
-constexpr int32_t maximum_mapping_entry_size{255};
+inline constexpr int32_t minimum_mapping_table_id{1};
+inline constexpr int32_t maximum_mapping_table_id{255};
+inline constexpr int32_t minimum_mapping_entry_size{1};
+inline constexpr int32_t maximum_mapping_entry_size{255};
 
-constexpr int32_t max_k_value{16}; // This is an implementation limit (theoretical limit is 32)
+inline constexpr int32_t max_k_value{16}; // This is an implementation limit (theoretical limit is 32)
 
 // ISO/IEC 14495-1, section 4.8.1 defines the SPIFF version numbers to be used for the SPIFF header in combination with
 // JPEG-LS.
-constexpr uint8_t spiff_major_revision_number{2};
-constexpr uint8_t spiff_minor_revision_number{};
+inline constexpr uint8_t spiff_major_revision_number{2};
+inline constexpr uint8_t spiff_minor_revision_number{};
 
-constexpr uint8_t spiff_end_of_directory_entry_type{1};
+inline constexpr uint8_t spiff_end_of_directory_entry_type{1};
 
 // The size of a SPIFF header when serialized to a JPEG byte stream.
-constexpr size_t spiff_header_size_in_bytes{34};
+inline constexpr size_t spiff_header_size_in_bytes{34};
 
 // The maximum size of the data bytes that fit in a spiff entry.
-constexpr size_t spiff_entry_max_data_size{65528};
+inline constexpr size_t spiff_entry_max_data_size{65528};
 
 // The special value to indicate that the stride should be calculated.
-constexpr size_t auto_calculate_stride{};
+inline constexpr size_t auto_calculate_stride{};
 
 // The size in bytes of the segment length field.
-constexpr size_t segment_length_size{sizeof(uint16_t)};
+inline constexpr size_t segment_length_size{sizeof(uint16_t)};
 
 // The maximum size of the data bytes that fit in a segment.
-constexpr size_t segment_max_data_size{std::numeric_limits<uint16_t>::max() - segment_length_size};
+inline constexpr size_t segment_max_data_size{std::numeric_limits<uint16_t>::max() - segment_length_size};
 
 // Number of bits in an int32_t data type.
-constexpr size_t int32_t_bit_count{sizeof(int32_t) * 8};
+inline constexpr size_t int32_t_bit_count{sizeof(int32_t) * 8};
 
 } // namespace charls
