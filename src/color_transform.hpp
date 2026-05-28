71a02e22fab858eaf7901fc53f475ce5640da9c3
Victor Derks <vbaderks@gmail.com>
2026-05-28 15:53:20 +0200

    fix: Make HP color transformations robust

--- src/color_transform.hpp
+++ src/color_transform.hpp
@@ -16,7 +16,10 @@ inline bool color_transformation_possible(const frame_info& frame) noexcept
 // They are used to convert between decoded values and the internal line buffers.
 // Color transforms work best for computer generated images, but are outside the official JPEG-LS specifications.
 
-
+// COLORXFORM_HP1 :
+// G = G
+// R = R - G
+// B = B - G
 template<typename SampleType>
 struct transform_hp1 final
 {
@@ -26,22 +29,28 @@ struct transform_hp1 final
 
     FORCE_INLINE triplet<SampleType> operator()(const int red, const int green, const int blue) const noexcept
     {
-        return {static_cast<SampleType>(red - green + range / 2), static_cast<SampleType>(green), static_cast<SampleType>(blue - green + range / 2)};
+        return {static_cast<SampleType>(red - green + bias), static_cast<SampleType>(green),
+                static_cast<SampleType>(blue - green + bias)};
     }
 
     struct inverse final
     {
         FORCE_INLINE triplet<SampleType> operator()(const int v1, const int v2, const int v3) const noexcept
         {
-            return {static_cast<SampleType>(v1 + v2 - range / 2), static_cast<SampleType>(v2), static_cast<SampleType>(v3 + v2 - range / 2)};
+            return {static_cast<SampleType>(std::clamp(v1 + v2 - bias, 0, range - 1)), static_cast<SampleType>(v2),
+                    static_cast<SampleType>(std::clamp(v3 + v2 - bias, 0, range - 1))};
         }
     };
 
 private:
-    static constexpr size_t range{1 << (sizeof(SampleType) * 8)};
+    static constexpr int range{1 << (sizeof(SampleType) * 8)};
+    static constexpr int bias{range / 2};
 };
 
-
+// COLORXFORM_HP2 :
+// G = G
+// B = B - (R + G) / 2
+// R = R - G
 template<typename SampleType>
 struct transform_hp2 final
 {
@@ -51,21 +60,23 @@ struct transform_hp2 final
 
     FORCE_INLINE triplet<SampleType> operator()(const int red, const int green, const int blue) const noexcept
     {
-        return {static_cast<SampleType>(red - green + range / 2), static_cast<SampleType>(green),
-                static_cast<SampleType>(blue - ((red + green) >> 1) - range / 2)};
+        return {static_cast<SampleType>(red - green + bias), static_cast<SampleType>(green),
+                static_cast<SampleType>(blue - ((red + green) / 2) - bias)}; // TODO: fix -> - to +?
     }
 
     struct inverse final
     {
         FORCE_INLINE triplet<SampleType> operator()(const int v1, const int v2, const int v3) const noexcept
         {
-            const auto r{static_cast<SampleType>(v1 + v2 - range / 2)};
-            return {r, static_cast<SampleType>(v2), static_cast<SampleType>(v3 + ((r + static_cast<SampleType>(v2)) >> 1) - range / 2)};
+            const auto r{v1 + v2 - bias};
+            return {static_cast<SampleType>(std::clamp(r, 0, range - 1)), static_cast<SampleType>(v2),
+                    static_cast<SampleType>(std::clamp(v3 + ((r + v2) >> 1) - bias, 0, range - 1))};
         }
     };
 
 private:
-    static constexpr size_t range{1 << (sizeof(SampleType) * 8)};
+    static constexpr int range{1 << (sizeof(SampleType) * 8)};
+    static constexpr int bias{range / 2};
 };
 
 
@@ -81,8 +92,8 @@ struct transform_hp3 final
         const auto v2{static_cast<SampleType>(blue - green + range / 2)};
         const auto v3{static_cast<SampleType>(red - green + range / 2)};
 
-        return {static_cast<SampleType>(green + ((v2 + v3) >> 2) - range / 4), static_cast<SampleType>(blue - green + range / 2),
-                static_cast<SampleType>(red - green + range / 2)};
+        return {static_cast<SampleType>(green + ((v2 + v3) >> 2) - range / 4),
+                static_cast<SampleType>(blue - green + range / 2), static_cast<SampleType>(red - green + range / 2)};
     }
 
     struct inverse final
@@ -90,7 +101,8 @@ struct transform_hp3 final
         FORCE_INLINE triplet<SampleType> operator()(const int v1, const int v2, const int v3) const noexcept
         {
             const auto g{static_cast<int>(v1 - ((v3 + v2) >> 2) + range / 4)};
-            return {static_cast<SampleType>(v3 + g - range / 2), static_cast<SampleType>(g), static_cast<SampleType>(v2 + g - range / 2)};
+            return {static_cast<SampleType>(v3 + g - range / 2), static_cast<SampleType>(g),
+                    static_cast<SampleType>(v2 + g - range / 2)};
         }
     };
 
